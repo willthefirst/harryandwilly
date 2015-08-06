2179bf1e38e0004b074bd17b0c41a456a9d2e90c
Will Moritz <will@crowdfavorite.com>
2015-08-06 16:25:45 -0400

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.initConfig ({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+
+    watch: {
+      source: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true, // needed to run LiveReload
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['sass']);
+};
