496ec1513beaecdca5dd10912749143b65286ecd
Will Moritz <will@crowdfavorite.com>
2015-09-03 07:34:48 -0400

    working baseball gifs

--- public/js/pages/just_baseball_gifs.js
+++ public/js/pages/just_baseball_gifs.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
+  var gif_array = [];
+  var gif_index = 0;
+  var random = Math.floor(Math.random() * 70) + 1
+
   $.ajax({
-    url: "http://api.giphy.com/v1/gifs/search?q=baseball&api_key=dc6zaTOxFJmzC&limit=100",
+    url: "http://api.giphy.com/v1/gifs/search?q=baseball&api_key=dc6zaTOxFJmzC&limit=100&offset=" + random,
     type: 'GET',
     dataType: 'json',
-  })
-  .done(function( object ) {
+  }).done(function( object ) {
     var data = object.data;
-    (".btn btn-default btn-lg").on("click", function(){
-      $('.gif-container').append("<img src='" + data[0].images.downsized_large.url + "' />");
+    console.log(data);
+    $(".btn btn-default btn-lg").on("click", function(){
       $(".btn btn-default btn-lg").hide()
       $(".btn btn deftault").show()
-      });
+    });
     for (var i = 0; i < data.length; i++) {
-      (".btn btn-default").on("click", function(){
-      $('.gif-container').append("<img src='" + data[i+1].images.downsized_large.url + "' />");
-      })
+      gif_array.push(data[i].images.original.url);
     }
-  });
-    
-  .fail(function( e ) {
+  }).fail(function( e ) {
+    $gif_container.prepend("Failed to retrieve gifs. Check console for error.");
     console.log('error' , e);
   });
+
+  $(".btn-default").on("click", function(){
+    console.log(gif_array[gif_index], gif_index);
+    $(".gif-container").html("<img src='" + gif_array[gif_index] + "' />");
+    gif_index = gif_index + 1;
+  })
 });
