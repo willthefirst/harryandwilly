564a390d255bd20f1167d2b7c13e3a8838eb286f
Will Moritz <will@crowdfavorite.com>
2015-09-08 16:09:23 -0400

    canvas drawing function good to go

--- public/js/pages/the_clock.js
+++ public/js/pages/the_clock.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
 
+  // Params
+  var square_dims = 300;
+  var spacing = 20;
+
+  var square_center = square_dims/2;
+
   // Initialize canvas
-  var canvas = document.getElementById("circles");
+  var canvas = document.getElementById("the-clock");
   var ctx = canvas.getContext("2d");
 
-  // Draws a circles
-  var drawCircle = function( x , y ) {
-    var radius = Math.floor(Math.random() * (150 - 1 +1 ) + 1);
+  // Set canvas dimensions
+  canvas.width = square_dims;
+  canvas.height = square_dims;
+
+  // Draws the progress
+  var drawClock = function( current , total , radius ) {
+      var percent = current/total;
       ctx.beginPath();
-      ctx.arc(x,y,radius,0,2*Math.PI);
+      ctx.arc(square_center,square_center,radius,-0.5*Math.PI,(percent*2*Math.PI - 0.5*Math.PI));
       ctx.stroke();
   }
+
+  // Update the canvas Clock
+  var updateCanvasClock = function( hour, minute, second ) {
+    drawClock( hour , 24 , square_center);
+    drawClock( minute , 60 , square_center-(spacing));
+    drawClock( second , 60 , square_center-(spacing*2));;
+  }
+
+  // Sample call of the updateClock function
+  updateCanvasClock(16,30,45);
 });
