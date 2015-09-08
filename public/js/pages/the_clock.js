b87e840b1cf776d58da3cf0578259f00990bc894
Will Moritz <will@crowdfavorite.com>
2015-09-08 16:18:06 -0400

    prettier clock

--- public/js/pages/the_clock.js
+++ public/js/pages/the_clock.js
@@ -2,23 +2,25 @@ $(document).ready(function() {
 
   // Params
   var square_dims = 300;
+  var padding = 30;
   var spacing = 20;
 
-  var square_center = square_dims/2;
+  var square_center = square_dims/2 + padding;
 
   // Initialize canvas
   var canvas = document.getElementById("the-clock");
   var ctx = canvas.getContext("2d");
 
   // Set canvas dimensions
-  canvas.width = square_dims;
-  canvas.height = square_dims;
+  canvas.width = square_dims + 2*padding;
+  canvas.height = square_dims + 2*padding;
 
   // Draws the progress
   var drawClock = function( current , total , radius ) {
       var percent = current/total;
       ctx.beginPath();
-      ctx.arc(square_center,square_center,radius,-0.5*Math.PI,(percent*2*Math.PI - 0.5*Math.PI));
+      ctx.arc(square_center,square_center,radius-padding,-0.5*Math.PI,(percent*2*Math.PI - 0.5*Math.PI));
+      ctx.lineWidth = 18;
       ctx.stroke();
   }
 
@@ -30,5 +32,5 @@ $(document).ready(function() {
   }
 
   // Sample call of the updateClock function
-  updateCanvasClock(16,30,45);
+  updateCanvasClock(24,30,45);
 });
