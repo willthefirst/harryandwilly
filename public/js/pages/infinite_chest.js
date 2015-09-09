66cbfa45ca2bd5d8b6dc2ed06e8fcfa34897ddfd
Will Moritz <will@crowdfavorite.com>
2015-09-08 23:55:08 -0400

    hacky fix for scrollTop

--- public/js/pages/infinite_chest.js
+++ public/js/pages/infinite_chest.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   window_height = $(document).height();
   var scroll_percent = 0;
   $(window).scroll(function(e){
-    scroll_percent = $(this).scrollTop()/window_height;
-    $(".pages-read").html(Math.floor(scroll_percent*1079));
-    $(".footnotes-read").html(Math.floor(scroll_percent*332));
+    scroll_percent = ($(this).scrollTop() + $(this).height())/window_height;
+    console.log($(this), window_height);
+    $(".pages-read").html(Math.round(scroll_percent*1079));
+    $(".footnotes-read").html(Math.round(scroll_percent*332));
   });
 });
