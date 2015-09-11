e22eb9b834fd6568c124cf6af965bf159d2c3c35
Will Moritz <will@crowdfavorite.com>
2015-09-11 16:27:13 -0400

    hopefully fixes image problem with infinite-chest

--- public/js/pages/infinite_chest.js
+++ public/js/pages/infinite_chest.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   var scroll_percent = 0;
   $(window).scroll(function(e){
     scroll_percent = ($(this).scrollTop() + $(this).height())/window_height;
-    console.log($(this), window_height);
     $(".pages-read").html(Math.round(scroll_percent*1079));
     $(".footnotes-read").html(Math.round(scroll_percent*332));
   });
