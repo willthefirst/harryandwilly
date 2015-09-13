8aac711072c9e36fc60192e36122a9fc4e3b0608
Harry DiFrancesco <hcdifrancesco@gmail.com>
2015-09-13 02:09:13 -0600

    fixes to which day

--- public/js/pages/which_day.js
+++ public/js/pages/which_day.js
@@ -4,17 +4,28 @@ $(document).ready(function() {
         var answer = "";
 
         // Gets the user's date.
-        var date = $('#datepicker_entry').val()
-        console.log(date);
+        var date = new Date($('#datepicker_entry').val());
+        date.setDate(date.getDate()+ 1);
+       console.log(date);
+        //error message
+        if (date.toString() === "Invalid Date") {
+            alert("Please Enter a Valid Date!")
+        } else {   
         var answer = date.getDay();
-
+        var displayAnswer;
         //object that stores days and codes
         var dayHolder = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var date = "December 25, 1995 23:15:00"
+        var displayAnswer = dayHolder[answer]
+
+        //Formatting adjustment of the date display
+        var displayDate =  date.toString();
+        var displayDate = displayDate.substring(4,15);
+    
         
-        console.log(date);
+        console.log(typeof displayDate);
 
         // Spits our answer back into the HTML.
-        $('.answer').html(date + ' is a ' + answer);
+        $('.answer').html(displayDate + ' is a ' +  displayAnswer);
+    }
     })
 });
