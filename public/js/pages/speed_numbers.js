043a4e98c5bfa2bed142aa5b8276d9554e27ffe8
Will Moritz <will@crowdfavorite.com>
2015-09-13 21:29:56 -0400

    one dumb motha fucking game

--- public/js/pages/speed_numbers.js
+++ public/js/pages/speed_numbers.js
@@ -1,97 +1,33 @@
 // From http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
 
 document.addEventListener("DOMContentLoaded", function(e) {
-  // Create the canvas
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  document.getElementById("game-container").appendChild(canvas);
 
   // Game objects
-  var dot = {
-  	x: canvas.width/2,
-  	y: canvas.height/2,
-    r: 40, // Radius
-    minRadius: 10,
-    maxRadius: 80,
-    color: "rgb(249, 205, 73)"
-  };
-
   var hits = 0;
   var misses = 0;
+  var score = 0;
+
+  var number = 0;
 
   // Event handling
-  addEventListener("click", function(e) {
-    // Is the click in the dot
-    if (Math.sqrt((e.x-dot.x)*(e.x-dot.x) + (e.y-dot.y)*(e.y-dot.y)) < dot.r) {
-      hits++;
+  $(document).on('keydown', function(e) {
+    // Reset the game when the player click a dot
+    if (String.fromCharCode(e.which) === number.toString()) {
       reset();
+      hits += 5;
     } else {
-      misses++;
+      misses += 10;
     }
-    console.log("Hits: " + hits + " | Misses: " + misses);
-  }, false);
+    score = Math.floor(hits - misses);
+    $(".score-number").html(score);
+  });
 
-  // Reset the game when the player click a dot
-  var reset = function () {
-    // Resize the dot
-    dot.r = Math.random() * (dot.maxRadius - dot.minRadius) + dot.minRadius;
-
-    // Color the dot
-    dot.color = getRandomColor();
-
-    // Move the dot
-    dot.x = Math.random() * ((canvas.width-dot.r) - dot.r) + dot.r;
-    dot.y = Math.random() * ((canvas.height-dot.r) - dot.r) + dot.r;
+  var reset = function () {  var score
+    number = Math.floor(Math.random() * 10);
+    $("#number").html(number).css("color", getRandomColor);
+    $("body").css("background-color", getRandomColor());
   };
 
-  // Draw everything
-  var render = function () {
-
-    // Draw background
-    ctx.fillStyle = "rgb(53, 219, 219)"
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-
-    // Draw dot
-    ctx.fillStyle = dot.color;
-    ctx.beginPath();
-    ctx.arc(dot.x, dot.y, dot.r , 0 , 2*Math.PI);
-    ctx.fill();
-    ctx.stroke();
-
-    // Draw score
-    ctx.fillStyle = "rgb(250, 250, 250)";
-    ctx.font = "14px Comic Sans MS";
-    ctx.textAlign = "left";
-    ctx.textBaseline = "top";
-    ctx.fillText("Hits: " + hits, 4, 0);
-    ctx.fillText("Misses: " + misses, 4, 18);
-
-
-  };
-
-  // Cross-browser support for requestAnimationFrame
-  var w = window;
-  requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
-
-  // The main game loop
-  var main = function () {
-  	var now = Date.now();
-  	var delta = now - then;
-
-  	render();
-
-  	then = now;
-
-  	// Request to do this again ASAP
-  	requestAnimationFrame(main);
-  };
-
-  // Let's play this game!
-  var then = Date.now();
-  reset();
-  main();
 });
 
 function getRandomColor() {
