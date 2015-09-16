f481c9e27edf400fe3815506a2bd1b3aec02c78a
Will Moritz <will@crowdfavorite.com>
2015-09-16 00:22:55 -0400

    circles are working

--- public/js/pages/something_freaky.js
+++ public/js/pages/something_freaky.js
@@ -1,5 +1,9 @@
 (function() {
 
+  // Params
+  var spacing = 100; // spacing between circles
+  var growthFactor = 5; // how quickly the circles grow
+
   // Initialize canvas
   var canvas = document.createElement("canvas");
   var ctx = canvas.getContext("2d");
@@ -9,14 +13,14 @@
   document.getElementsByTagName('body')[0].appendChild(canvas);
 
   colors = [
-    "#000", "#e0e0e0", "rgb(63, 217, 115)"
+    "#000", "#fff", "red"
   ]
 
   // Alternates colors of drawn circles.
   var colorIndex = 0;
   var nextColor = function() {
     var color = "";
-    if (colorIndex >= colors.length) {
+    if (colorIndex >= (colors.length-1)) {
       colorIndex = 0;
     } else {
       colorIndex++;
@@ -29,26 +33,22 @@
   // Grow a circle
   var drawIt = function(color) {
     var i = 0;
+    console.log("Circle #: " + circles + " color= " + color);
     return function draw() {
-      // Stop drawing once the circle is big enough
-      // ctx.clearRect(0,0,canvas.width,canvas.height);
       ctx.fillStyle = color;
       ctx.beginPath();
       ctx.arc(canvas.width/2, canvas.height/2, i, 0, 2 * Math.PI);
       ctx.fill();
-      console.log('bang');
-      i+=1;
+      i+=1*growthFactor;
+      // Growing circles until they are huge
       if (i < canvas.width) {
         window.requestAnimationFrame(draw);
-      } else {
-        circles++;
-        console.log(circles);
+        if (i === spacing) {
+          circles++
+          drawIt(nextColor())();
+        }
       }
     }
-
-
   };
-  var go =  drawIt(nextColor());
-  window.setInterval(go,3000);
-
+  drawIt(nextColor())();
 })();
